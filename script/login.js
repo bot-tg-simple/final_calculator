f734ef6354686b76fa4a8b31a5630d46cbefdf27
bot-tg-simple <144138955+bot-tg-simple@users.noreply.github.com>
2024-04-21 19:40:21 +0300

    Create login.js

---
+++ script/login.js
@@ -0,0 +1,54 @@
+const form = document.querySelector("form");
+const input_login = document.getElementById("login");
+const input_password = document.getElementById("password")
+const results = document.querySelector(".form__message");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); // Предотвращаем стандартное действие браузера
+
+    const login = input_login.value;
+    const password = input_password.value;
+
+    if (login === '' || password === '') {
+        results.innerHTML = `<p class="form__message">Поля логина и пароля должны быть заполнены</p>`;
+        return
+    }
+    
+    fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            login: login,
+            password: password
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('result:', data.authentication);
+        results.innerHTML = `<p class="form__message">${data.authentication}</p>`;
+        if (data.JWT) {
+            RemoveJWT();
+            PutJWT(data.JWT);
+        }
+    })
+    .catch((error) => {
+        alert(`Error: ${error}\n
+        Скорее всего вы просто не запустили сервер`);
+        console.error('Error:', error);
+    });
+
+    // Очистка поля ввода после отправки
+    input_login.value = '';
+    input_password.value = '';
+});
+
+
+function PutJWT(jwt) {
+    localStorage.setItem('jwt', jwt)
+}
+
+function RemoveJWT() {
+    localStorage.removeItem('jwt')
+}
